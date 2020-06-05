c834e2b917bc724944689b7af7f936aa4100f5ca
WDNinja515 <63159330+WDNinja515@users.noreply.github.com>
2020-06-05 13:12:08 -0300

    Add files via upload

---
+++ src/main/java/wdns3dnm/wdns3dnm/BlockList.java
@@ -0,0 +1,13 @@
+package wdns3dnm.wdns3dnm;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+
+public class BlockList extends Block
+{
+   public static Block sasuke_susanoo_perfect;
+
+   public BlockList(Properties p_i48487_1_) {
+      super(p_i48487_1_);
+   }
+}
---
+++ src/main/java/wdns3dnm/wdns3dnm/WDNs3DNM.java
@@ -0,0 +1,42 @@
+package wdns3dnm.wdns3dnm;
+
+import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
+@Mod("wdns3dnm")
+public class WDNs3DNM {
+    public static WDNs3DNM instance;
+    public static final String MOD_ID = "wdns3dnm";
+    public static final Logger LOGGER = LogManager.getLogger();
+    private final Object StringReader = new Object();
+
+    public WDNs3DNM() {
+
+        instance = this;
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+    }
+
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+
+    }
+
+    public void onServerStarting(FMLServerStartingEvent event) {
+    }
+
+
+}
